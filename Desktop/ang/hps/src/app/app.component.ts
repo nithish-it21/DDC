ea357c917b310cd784b22dbbe36d1ca6245b6d26
nithish.it21 <nithish.it21@bitsathy.ac.in>
2023-11-03 08:38:13 +0530

    first commit

---
+++ Desktop/ang/hps/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./css/animate.css','./css/aos.css',
+                './css/bootstrap.min.css','./css/flaticon.css','./css/icomoon.css',
+                './css/ionicons.min.css'
+                ,'./css/magnific-popup.css','./css/open-iconic-bootstrap.min.css'
+                ,'./css/owl.theme.default.min.css','./css/bootstrap/bootstrap-grid.css',
+              './css/bootstrap/bootstrap-reboot.css','./css/style.css']
+})
+export class AppComponent {
+  title = 'hps';
+}
